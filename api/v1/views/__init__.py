ee42da5d98509114e71e4a9939ba3e12d4796d01
Bsakwa <sakwabrian23@gmail.com>
2023-05-25 21:59:50 +0300

    Add __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,8 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.categories import *
+from api.v1.views.users import *
+from api.v1.views.service_providers import *
+from api.v1.views.locations import *
+from api.v1.views.reviews import *
