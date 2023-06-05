81c7b2314c34bf9dd4b1190f71242d84cf8ccd91
Bsakwa <sakwabrian23@gmail.com>
2023-06-05 18:16:50 +0300

    Added Form submit logic

--- huduma-app/src/LoginPage.js
+++ huduma-app/src/LoginPage.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button, TextField } from '@mui/material';
+import axios from 'axios';
 import './styles/LoginPage.css';
 
 // Import your custom icon images
@@ -8,9 +10,47 @@ import microsoftIcon from './assets/microsoft.png';
 import appleIcon from './assets/apple.png';
 
 const LoginPage = () => {
-  const handleFormSubmit = (provider) => {
-    // Handle form submission for different providers
-    console.log(`Continue with ${provider}`);
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+    setEmailError('');
+  };
+
+  const validateEmail = () => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address');
+      return false;
+    }
+    return true;
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    if (!validateEmail()) {
+      return;
+    }
+
+    try {
+      const response = await axios.get(`http://localhost:5000/api/v1/users?email=${email}`);
+      console.log('Response:', response);
+
+      if (response.status === 200) {
+        // User exists in the database
+        console.log('User exists in the database');
+        navigate('/home', { state: { email: email } });
+      } else {
+        // User does not exist in the database
+        console.log('User does not exist in the database');
+      }
+    } catch (error) {
+      console.log('Error occurred while validating email:', error.message);
+      // Add your logic to display an error message or handle the error accordingly
+    }
   };
 
   return (
@@ -18,14 +58,24 @@ const LoginPage = () => {
       <div className="login-page">
         <h2>Login</h2>
 
-        <form className="login-form">
-          <TextField id="email" type="email" label="Email" required fullWidth />
-
+        <form className="login-form" onSubmit={handleFormSubmit}>
+          <TextField
+            name="email"
+            type="email"
+            label="Email"
+            required
+            fullWidth
+            value={email}
+            onChange={handleChange}
+            error={!!emailError}
+            helperText={emailError}
+          />
+	  
           <div className="button-container">
-	  <Button type="submit" variant="contained" className="login-button">
-            Login
-          </Button>
-	  </div>
+            <Button type="submit" variant="contained" className="login-button">
+              Login
+            </Button>
+          </div>
         </form>
 
         <div className="continue-container">
@@ -47,7 +97,7 @@ const LoginPage = () => {
 
           <form onSubmit={() => handleFormSubmit('Apple')}>
             <Button type="submit" variant="contained" className="continue-button">
-              <img src={appleIcon}  alt="Apple" className="continue-icon" />
+              <img src={appleIcon} alt="Apple" className="continue-icon" />
               Continue with Apple
             </Button>
           </form>
