5a3e986ff5a0223ae2ad5b9aecfb5629ba2478b4
Bsakwa <sakwabrian23@gmail.com>
2023-06-06 15:42:11 +0300

    Ensured users signed up on the platform can login to the home page

--- huduma-app/src/LoginPage.js
+++ huduma-app/src/LoginPage.js
@@ -36,16 +36,37 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await axios.get(`http://localhost:5000/api/v1/users?email=${email}`);
-      console.log('Response:', response);
+      // Get all user emails
+      const usersResponse = await axios.get('http://localhost:5000/api/v1/users');
+      console.log('Users Response:', usersResponse);
 
-      if (response.status === 200) {
-        // User exists in the database
-        console.log('User exists in the database');
-        navigate('/home', { state: { email: email } });
+      // Get all service provider emails
+      const providersResponse = await axios.get('http://localhost:5000/api/v1/service_providers');
+      console.log('Service Providers Response:', providersResponse);
+
+      if (usersResponse.status === 200 && providersResponse.status === 200) {
+        const userEmails = usersResponse.data.map((user) => user.email);
+        const providerEmails = providersResponse.data.map((provider) => provider.email);
+        const allEmails = [...userEmails, ...providerEmails];
+
+        if (allEmails.includes(email)) {
+          // Email exists in either users or service providers
+          console.log('Email exists in the database');
+
+          // Determine the userType based on the email match
+          const userType = userEmails.includes(email) ? 'user' : 'serviceProvider';
+
+          navigate('/home', { state: { email: email, userType: userType } });
+          return;
+        }
+
+        // Email does not exist in the database
+        console.log('Email does not exist in the database');
+        setEmailError('Invalid password entered. Please try again.');
+	// Add your logic to display an error message or handle the case accordingly
       } else {
-        // User does not exist in the database
-        console.log('User does not exist in the database');
+        console.log('Error occurred while retrieving emails:', usersResponse.status, providersResponse.status);
+        // Add your logic to display an error message or handle the error accordingly
       }
     } catch (error) {
       console.log('Error occurred while validating email:', error.message);
@@ -70,7 +91,7 @@ const LoginPage = () => {
             error={!!emailError}
             helperText={emailError}
           />
-	  
+
           <div className="button-container">
             <Button type="submit" variant="contained" className="login-button">
               Login
