2174daba1938076d80eb739bf81ba48efb9282d5
Bsakwa <sakwabrian23@gmail.com>
2023-06-08 20:30:22 +0300

    Added new Header.js

--- huduma-app/src/Header.js
+++ huduma-app/src/Header.js
@@ -10,19 +10,16 @@ const Header = () => {
         <div className="logo">
         </div>
         <div className="slogan">
-         Accessibility. Visibility. Service.
+          Accessibility. Visibility. Service.
         </div>
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-          <SearchIcon className="search-icon" />
-        </div>
-        <ul className="nav-links">
-	  <li><Link to="/about">About</Link></li>
-	  <li><Link to="/services">Services</Link></li>
-	  <li><Link to="/contact">Contact</Link></li>
-	  <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
+        <div> {/* Added opening div tag */}
+          <ul className="nav-links">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </div> {/* Removed extra closing div tag */}
       </nav>
     </header>
   );
