ce3955948cf7795a4c0e435b8a3e23c693088c9e
Bsakwa <sakwabrian23@gmail.com>
2023-06-09 17:30:44 +0300

    Tried to set up user authentication and session management

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,12 +9,29 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from os import getenv
-
+from flask_session import Session
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.secret_key = 'your_secret_key_here'
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Configure Flask Session
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_FILE_THRESHOLD'] = 100
+app.config['SESSION_FILE_MODE'] = 384
+app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
+app.config['SESSION_COOKIE_NAME'] = 'flask_session'
+app.config['SESSION_COOKIE_DOMAIN'] = None
+app.config['SESSION_COOKIE_PATH'] = None
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SECURE'] = False
+app.config['SESSION_REFRESH_EACH_REQUEST'] = True
+sess = Session()
+sess.init_app(app)
 
 
 @app.teardown_appcontext
@@ -22,12 +39,10 @@ def teardown_appcontext(exception):
     """Closes the storage"""
     storage.close()
 
-
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 error"""
     return make_response(jsonify({'error': 'Not found'}), 404)
-
 
 if __name__ == "__main__":
     host = getenv('HUDUMA_API_HOST')
